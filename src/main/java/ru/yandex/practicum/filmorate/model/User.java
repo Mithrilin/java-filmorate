5f7cc6f4ba83e2521ec5be75f9f3d92736e6ddb4
Roman <romanukyan86@yandex.ru>
2023-11-25 17:07:44 +0300

    создать классы-модели

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,27 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
+import java.time.LocalDate;
+
+@Data
+public class User {
+    private Integer id;
+    @Email
+    private final String email;
+    @NotBlank
+    private final String login;
+    private String name;
+    @Past
+    private final LocalDate birthday;
+
+    public User(String email, String login, String name, LocalDate birthday) {
+        this.email = email;
+        this.login = login;
+        this.name = name;
+        this.birthday = birthday;
+    }
+}

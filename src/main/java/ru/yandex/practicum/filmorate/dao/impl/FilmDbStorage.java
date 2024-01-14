63f5136e1bc9d4e4319aa1fa635d1aa1250618e5
Roman <romanukyan86@yandex.ru>
2024-01-14 23:14:56 +0300

    реализовать метод добавления фильма

--- src/main/java/ru/yandex/practicum/filmorate/dao/impl/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/impl/FilmDbStorage.java
@@ -1,14 +1,35 @@
 package ru.yandex.practicum.filmorate.dao.impl;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 
 import java.util.Map;
+import java.util.Objects;
 
+@Component("filmDbStorage")
 public class FilmDbStorage implements FilmStorage {
+    private final JdbcTemplate jdbcTemplate;
+
+    public FilmDbStorage(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public Film addFilm(Film film) {
-        return null;
+        SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert(Objects.requireNonNull(jdbcTemplate.getDataSource()))
+                .withTableName("films")
+                .usingGeneratedKeyColumns("id");
+        Map<String, String> params = Map.of(
+                "name", film.getName(),
+                "releaseDate", film.getReleaseDate().toString(),
+                "description", film.getDescription(),
+                "duration", film.getDuration().toString(),
+                "mpa_id", film.getMpa().getId().toString());
+        film.setId(simpleJdbcInsert.executeAndReturnKey(params).intValue());
+        return film;
     }
 
     @Override

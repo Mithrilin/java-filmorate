0efb1d7f15412ef8269e566cfa22fa30f7c3b67c
Roman <romanukyan86@yandex.ru>
2024-01-14 17:12:54 +0300

    добавить методы возвращения mpa по ид и возвращения всех mpa

--- src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
@@ -1,11 +1,28 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.model.Mpa;
+import ru.yandex.practicum.filmorate.service.mpa.MpaService;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/mpa")
 public class MpaController {
+    private final MpaService mpaService;
+
+    @GetMapping("/{id}")
+    public Mpa getMpaById(@PathVariable int id) {
+        return mpaService.getMpaById(id);
+    }
+
+    @GetMapping
+    public List<Mpa> getAllMpa() {
+        return mpaService.getAllMpa();
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/mpa/MpaServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/mpa/MpaServiceImpl.java
@@ -1,17 +1,30 @@
 package ru.yandex.practicum.filmorate.service.mpa;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.dao.MpaDao;
 import ru.yandex.practicum.filmorate.model.Mpa;
 
 import java.util.List;
 
+@Slf4j
+@Service
+@AllArgsConstructor
 public class MpaServiceImpl implements MpaService{
+    private final MpaDao mpaDao;
+
     @Override
     public Mpa getMpaById(int id) {
-        return null;
+        Mpa mpa = mpaDao.getMpaById(id);
+        log.info("Mpa с id {} возвращён.", id);
+        return mpa;
     }
 
     @Override
     public List<Mpa> getAllMpa() {
-        return null;
+        List<Mpa> mpas = mpaDao.getAllMpa();
+        log.info("Текущее количество mpa: {}. Список возвращён.", mpas.size());
+        return mpas;
     }
 }

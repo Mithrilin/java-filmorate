38b5a9e0bf120ae097b19bca55d68b260bd2fe1c
Roman <romanukyan86@yandex.ru>
2024-01-19 18:30:32 +0300

    добавить метод testUpdateFilmShouldBeEquals

--- src/test/java/ru/yandex/practicum/filmorate/dao/impl/FilmDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/dao/impl/FilmDbStorageTest.java
@@ -13,38 +13,59 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @JdbcTest
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 class FilmDbStorageTest {
-    private static final Integer MPA_ID = 1;
-    private static final String MPA_NAME = "G";
-    private static final Integer GENRE_ID = 1;
-    private static final String GENRE_NAME = "Комедия";
-    private static Genre genreOne = new Genre();
-    private static final String NAME_FILM_ONE = "Тестовый фильм";
-    private static final String DESCRIPTION = "Описание тестового фильма";
+    private static final Integer MPA_ID_ONE = 1;
+    private static final String MPA_NAME_ONE = "G";
+    private static final Integer MPA_ID_TWO = 2;
+    private static final String MPA_NAME_TWO = "PG";
+    private static final Integer GENRE_ID_ONE = 1;
+    private static final String GENRE_NAME_ONE = "Комедия";
+    private static final Integer GENRE_ID_TWO = 2;
+    private static final String GENRE_NAME_TWO = "Драма";
+    private static final Genre genreOne = new Genre();
+    private static final Genre genreTwo = new Genre();
+    private static final String NAME_FILM_ONE = "Тестовый фильм один";
+    private static final String NAME_FILM_TWO = "Тестовый фильм два";
+    private static final String DESCRIPTION_FILM_ONE = "Описание первого тестового фильма";
+    private static final String DESCRIPTION_FILM_TWO = "Описание второго тестового фильма";
     private static final LocalDate RELEASE_DATE_FILM_ONE = LocalDate.of(1986, 10, 25);
+    private static final LocalDate RELEASE_DATE_FILM_TWO = LocalDate.of(1974, 03, 14);
     private static final Integer DURATION_FILM_ONE = 120;
-    private static final Mpa mpa = new Mpa();
+    private static final Integer DURATION_FILM_TWO = 100;
+    private static final Mpa mpaOne = new Mpa();
+    private static final Mpa mpaTwo = new Mpa();
     private static final Integer INITIAL_LIKE = 0;
     private static Film filmOne = null;
+    private static Film filmTwo = null;
     private FilmDbStorage filmDbStorage;
     private final JdbcTemplate jdbcTemplate;
 
 
     @BeforeAll
     static void beforeAll() {
-        mpa.setId(MPA_ID);
-        mpa.setName(MPA_NAME);
-        genreOne.setId(GENRE_ID);
-        genreOne.setName(GENRE_NAME);
-        List<Genre> genres = new ArrayList<>();
-        genres.add(genreOne);
-        filmOne = new Film(NAME_FILM_ONE, DESCRIPTION, RELEASE_DATE_FILM_ONE, DURATION_FILM_ONE, mpa);
+        mpaOne.setId(MPA_ID_ONE);
+        mpaOne.setName(MPA_NAME_ONE);
+        genreOne.setId(GENRE_ID_ONE);
+        genreOne.setName(GENRE_NAME_ONE);
+        List<Genre> genresFilmOne = new ArrayList<>();
+        genresFilmOne.add(genreOne);
+        filmOne = new Film(NAME_FILM_ONE, DESCRIPTION_FILM_ONE, RELEASE_DATE_FILM_ONE, DURATION_FILM_ONE, mpaOne);
         filmOne.setLike(INITIAL_LIKE);
-        filmOne.setGenres(genres);
+        filmOne.setGenres(genresFilmOne);
+
+        mpaTwo.setId(MPA_ID_TWO);
+        mpaTwo.setName(MPA_NAME_TWO);
+        genreTwo.setId(GENRE_ID_TWO);
+        genreTwo.setName(GENRE_NAME_TWO);
+        List<Genre> genresFilmTwo = new ArrayList<>();
+        genresFilmTwo.add(genreTwo);
+        filmTwo = new Film(NAME_FILM_TWO, DESCRIPTION_FILM_TWO, RELEASE_DATE_FILM_TWO, DURATION_FILM_TWO, mpaOne);
+        filmTwo.setLike(INITIAL_LIKE);
+        filmTwo.setGenres(genresFilmTwo);
     }
 
     @BeforeEach
@@ -54,9 +75,7 @@ class FilmDbStorageTest {
 
 
 
-    @Test
-    void updateFilm() {
-    }
+
 
     @Test
     void getFilmById() {
@@ -81,11 +100,25 @@ class FilmDbStorageTest {
     @Test
     @DisplayName("Добавление фильма")
     void testAddFilmShouldBeEquals() {
-        int expectedId = 1;
+        int filmId = 1;
 
         filmDbStorage.addFilm(filmOne);
-        Film savedFilm = filmDbStorage.getFilmById(expectedId).get(0);
+        Film savedFilm = filmDbStorage.getFilmById(filmId).get(0);
 
+        assertNotNull(savedFilm);
         assertEquals(filmOne, savedFilm);
+    }
+
+    @Test
+    @DisplayName("Обновление фильма")
+    void testUpdateFilmShouldBeEquals() {
+        Film savedFilm = filmDbStorage.addFilm(filmOne);
+        filmTwo.setId(savedFilm.getId());
+
+        filmDbStorage.updateFilm(filmTwo);
+        Film updatedFilm = filmDbStorage.getFilmById(savedFilm.getId()).get(0);
+
+        assertNotNull(updatedFilm);
+        assertEquals(filmTwo, updatedFilm);
     }
 }

6e85daa0e7ebc46d03f41a104a2ab83396192603
Roman <romanukyan86@yandex.ru>
2024-01-20 00:19:35 +0300

    добавить метод testGetGenreByIdShouldBeEquals

--- src/test/java/ru/yandex/practicum/filmorate/dao/impl/GenreDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/dao/impl/GenreDbStorageTest.java
@@ -2,10 +2,13 @@ package ru.yandex.practicum.filmorate.dao.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
+import ru.yandex.practicum.filmorate.model.Genre;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,13 +16,24 @@ import static org.junit.jupiter.api.Assertions.*;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 class GenreDbStorageTest {
+    private static final Integer GENRE_ID_ONE = 1;
+    private static final String GENRE_NAME_ONE = "Комедия";
+    private static Genre genreOne = null;
+    private GenreDbStorage genreDbStorage;
+    private final JdbcTemplate jdbcTemplate;
 
     @BeforeEach
     void setUp() {
+        genreDbStorage = new GenreDbStorage(jdbcTemplate);
+        genreOne = new Genre(GENRE_ID_ONE, GENRE_NAME_ONE);
     }
 
     @Test
-    void getGenreById() {
+    @DisplayName("Получение жанра по ид")
+    void testGetGenreByIdShouldBeEquals() {
+        Genre genre = genreDbStorage.getGenreById(GENRE_ID_ONE).get(0);
+
+        assertEquals(genreOne, genre);
     }
 
     @Test

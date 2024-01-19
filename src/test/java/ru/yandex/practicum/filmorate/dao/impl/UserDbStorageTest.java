96ac059578c896b1a58f752591d18b0d2345b9c1
Roman <romanukyan86@yandex.ru>
2024-01-19 22:08:34 +0300

    создать тестовый класс UserDbStorageTest

---
+++ src/test/java/ru/yandex/practicum/filmorate/dao/impl/UserDbStorageTest.java
@@ -0,0 +1,54 @@
+package ru.yandex.practicum.filmorate.dao.impl;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDbStorageTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void addUser() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void getUserById() {
+    }
+
+    @Test
+    void getAllUsers() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+
+    @Test
+    void addFriend() {
+    }
+
+    @Test
+    void deleteFriend() {
+    }
+
+    @Test
+    void getAllFriends() {
+    }
+
+    @Test
+    void getAllCommonFriends() {
+    }
+}

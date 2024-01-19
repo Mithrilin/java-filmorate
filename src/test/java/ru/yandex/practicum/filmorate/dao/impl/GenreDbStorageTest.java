aced7b263801b9dc6493ca1f1c465982a4edee7b
Roman <romanukyan86@yandex.ru>
2024-01-19 23:25:07 +0300

    добавить тестовый класс GenreDbStorageTest

---
+++ src/test/java/ru/yandex/practicum/filmorate/dao/impl/GenreDbStorageTest.java
@@ -0,0 +1,28 @@
+package ru.yandex.practicum.filmorate.dao.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@JdbcTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+class GenreDbStorageTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getGenreById() {
+    }
+
+    @Test
+    void getAllGenres() {
+    }
+}

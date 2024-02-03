3bdcdb21becc1b1b26ae15c84f553bfd829bfa98
Valerianjones1 <dinkiev.valery@yandex.ru>
2024-02-03 19:11:35 +0300

    fix: Убрал ненужный импорт

--- src/main/java/ru/yandex/practicum/filmorate/service/event/EventServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/event/EventServiceImpl.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.service.event;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.dao.EventDao;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Event;
 
 import java.util.List;
